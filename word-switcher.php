de86b8fe5d4c7ed7dd57e43a31c11ee4670d61d2
mrinal013 <mrinalhaque99@gmail.com>
2026-01-13 16:11:39 +0600

    WS - instruction added

--- word-switcher.php
+++ word-switcher.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:         Word Switcher
  * Description:         Add word swithing capability.
- * Version:             0.1.0
+ * Version:             1.0.0
  * Requires at least:   6.7
  * Requires PHP:        7.4
  * Author:              mrinal013
